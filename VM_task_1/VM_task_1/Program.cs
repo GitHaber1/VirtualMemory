910bc30c19c180f651d01993b62193d5f3c69cec
GitHaber1 <badindan2003@gmail.com>
2023-02-20 10:33:22 +0700

    Fix code

--- VM_task_1/VM_task_1/Program.cs
+++ VM_task_1/VM_task_1/Program.cs
@@ -21,23 +21,33 @@ namespace VM_task_1
             // Переменная для случайного заполнения ячеек.
             int randomSym;
             // Заполняем отедльные ячейки.
-            if (gen.WriteElement(522, 120) == -1) Console.WriteLine("Ошибка записи!");
-            if (gen.WriteElement(523, 120) == -1) Console.WriteLine("Ошибка записи!");
-            if (gen.WriteElement(555, 120) == -1) Console.WriteLine("Ошибка записи!");
-            if (gen.WriteElement(530, 120) == -1) Console.WriteLine("Ошибка записи!");
+            if (gen.WriteElement(522, 120) == -1)
+                Console.WriteLine("Ошибка записи!");
+            if (gen.WriteElement(523, 120) == -1)
+                Console.WriteLine("Ошибка записи!");
+            if (gen.WriteElement(555, 120) == -1)
+                Console.WriteLine("Ошибка записи!");
+            if (gen.WriteElement(530, 120) == -1)
+                Console.WriteLine("Ошибка записи!");
             // Проверка, если недостаточно памяти для размещения элемента.
-            if (gen.WriteElement(10500, 120) == -1) Console.WriteLine("Ошибка записи!");
+            if (gen.WriteElement(10500, 120) == -1)
+                Console.WriteLine("Ошибка записи!");
             // Для наглядности заполняем подряд 4 страницы.
             for (int i = 0; i < 512; i++)
             {
                 randomSym = rand.Next(2, 1000);
                 // Вывод элемента массива до записи в него.
-                if (gen.ReadElement(i) == -1) Console.WriteLine("Ошибка чтения!");
-                else Console.WriteLine(gen.ReadElement(i) + " - до записи");
-                if (gen.WriteElement(i, randomSym) == -1) Console.WriteLine("Ошибка записи!");
+                if (gen.ReadElement(i) == -1)
+                    Console.WriteLine("Ошибка чтения!");
+                else
+                    Console.WriteLine(gen.ReadElement(i) + " - до записи");
+                if (gen.WriteElement(i, randomSym) == -1)
+                    Console.WriteLine("Ошибка записи!");
                 // Вывод элемента после записи в него.
-                if (gen.ReadElement(i) == -1) Console.WriteLine("Ошибка чтения!");
-                else Console.WriteLine(gen.ReadElement(i) + " - после записи");
+                if (gen.ReadElement(i) == -1)
+                    Console.WriteLine("Ошибка чтения!");
+                else
+                    Console.WriteLine(gen.ReadElement(i) + " - после записи");
             }
         }
         // Точка входа в программу.
@@ -47,5 +57,5 @@ namespace VM_task_1
             Enter enter = new Enter();
             enter.TestRun(gen);
         }
-    }    
+    }
 }
